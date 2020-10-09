b8b937c5cf0de204d579cb8ecac34ddecf610fd9
brianyu2 <59840881+brianyu2@users.noreply.github.com>
2020-10-09 18:43:05 -0500

    Initial commit

---
+++ Assets/StateChange.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StateChange : MonoBehaviour
+{
+    Animator anima;
+    public AudioSource mySound;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        anima = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            anima.SetTrigger("sayhi");
+            mySound.Play();
+        }
+        if (Input.GetKeyDown(KeyCode.D))
+        {
+            anima.SetTrigger("dodance");
+        }
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+            anima.SetTrigger("sitdown");
+        }
+    }
+}
